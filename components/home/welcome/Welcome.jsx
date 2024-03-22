734d45bdaa56f6a4c066bf03046c8c5ba40f833e
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2024-03-22 11:26:58 -0700

    update welcome and popular jobs

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -44,15 +44,22 @@ const Welcome = () => {
       </View>
 
       <View style={styles.tabsContainer}>
-        <FileList
+        <FlatList
           data={jobTypes}
           renderItem={({ item }) => (
             <TouchableOpacity
               style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
             >
-              <Text>{item}</Text>
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
             </TouchableOpacity>
           )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small}}
+          horizontal
         />
       </View>
     </View>
